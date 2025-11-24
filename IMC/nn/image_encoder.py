c414a9ccad9fb23ab25e16e92b90ddf4e183a674
Tuan Truong <tuan.truong@bayer.com>
2025-11-24 16:08:04 +0000

    Change input dim

--- IMC/nn/image_encoder.py
+++ IMC/nn/image_encoder.py
@@ -96,7 +96,8 @@ class MultiSliceImageEncoder(nn.Module):
         Returns:
             torch.Tensor: Slice embeddings of shape (B, N_slices, slice_feat_dim)
         """
-        B, N, C, H, W = x.shape
+        B, N, H, W = x.shape
+        C = 1 # For grayscale images
         x = x.view(B * N, C, H, W)  # treat slices as batch
         
         features = self.cnn(x)  # For ResNet18 (B*N, 512, H', W'), for DenseNet121 (B*N, 1024, H', W')
