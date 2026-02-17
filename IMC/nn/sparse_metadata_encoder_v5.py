a0bb71aad1736209dbffe81e1412bd770521ac21
Matthias Lenga <46369191+MatthiasLen@users.noreply.github.com>
2026-02-17 16:23:49 +0100

    Update embed_dim calculation in sparse_metadata_encoder
    Refactor embed_dim calculation to include a base value of 32 and clarify heuristic choice.

--- IMC/nn/sparse_metadata_encoder_v5.py
+++ IMC/nn/sparse_metadata_encoder_v5.py
@@ -100,8 +100,9 @@ class SparseMetadataEncoder(nn.Module):
         # This we do not adress here and use a rather weak estimate based on num_features.
         # Can be investigated later.
         
-        # 1) embed_dim scales sublinearly
-        embed_dim = int(32 * round(math.sqrt(num_features) / 32))
+        # 1) embed_dim scales sublinearly ~ sqrt(num_features).
+        # This is a VERY Heuristic choice and 
+        embed_dim = 32 + int(32 * round(math.sqrt(num_features) / 32))
         embed_dim = max(64, min(embed_dim, 256))
     
         # 2) depth capped small
